fe625db45dc9c96f443667296e7d05d2cfb204a1
Suryansh Dey <deysuryansh@gmail.com>
2026-01-28 20:55:09 +0530

    Completing the API without auth

--- src/function.rs
+++ src/function.rs
@@ -1,18 +1,89 @@
-use std::vec;
-use gemini_client_api::gemini::{
-    ask::Gemini,
-    types::{request::Tool, sessions::Session}, utils::GeminiSchema,
+use crate::{
+    api_requests::{
+        flights::amadeus::{flight_seats_available, flights_between},
+        hotels::amadeus::hotels_in_city,
+        site_seen::get_about_place,
+        trains::rapidapi::{train_seats_available, trains_between},
+    },
+    constants::TRAVEL_PLANNER_SYS_PROMPT,
 };
+use gemini_client_api::{
+    futures::TryFutureExt,
+    gemini::{
+        ask::Gemini,
+        error::GeminiResponseError,
+        types::{request::Tool, response::GeminiResponseStream, sessions::Session},
+        utils::{GeminiSchema, execute_function_calls},
+    },
+};
+use serde::{Deserialize, Serialize};
+use serde_json::from_str;
+use std::{fmt::Display, vec};
 
-use crate::{api_requests::{flights::amadeus::{flight_seats_available, flights_between}, hotels::amadeus::hotels_in_city, site_seen::get_about_place, trains::rapidapi::{train_seats_available, trains_between}}, constants::TRAVEL_PLANNER_SYS_PROMPT};
+#[derive(Serialize, Deserialize)]
+pub struct ApiRequest {
+    pub session: Session,
+}
 
-pub async fn plan_tour(
-    session: Session,
-) -> Result<Session, Box<dyn std::error::Error + Send + Sync>> {
+async fn plan_tour(mut session: Session) -> Result<GeminiResponseStream, GeminiResponseError> {
+    let tools = vec![
+        hotels_in_city::gemini_schema(),
+        flights_between::gemini_schema(),
+        flight_seats_available::gemini_schema(),
+        trains_between::gemini_schema(),
+        train_seats_available::gemini_schema(),
+        get_about_place::gemini_schema(),
+    ];
     let ai = Gemini::new(
         std::env::var("GEMINI_API_KEY").unwrap(),
         "gemini-3-flash-preview",
         Some(TRAVEL_PLANNER_SYS_PROMPT.into()),
-    ).set_tools(vec![Tool::FunctionDeclarations(vec![hotels_in_city::gemini_schema(), flights_between::gemini_schema(), flight_seats_available::gemini_schema(), trains_between::gemini_schema(), train_seats_available::gemini_schema(), get_about_place::gemini_schema()])]);
-    todo!()
+    )
+    .set_tools(vec![Tool::FunctionDeclarations(tools.clone())]);
+    let results = execute_function_calls!(
+        session,
+        hotels_in_city,
+        flights_between,
+        flight_seats_available,
+        train_seats_available,
+        trains_between,
+        get_about_place
+    );
+    for i in 0..tools.len() {
+        if let Some(Err(e)) = &results[i] {
+            session
+                .add_function_response(
+                    flights_between::name(&tools[i]).unwrap(),
+                    serde_json::json!({"Error":e}),
+                )
+                .unwrap();
+        }
+    }
+    ai.ask_as_stream(session).await.map_err(|e| e.1)
+}
+
+#[derive(Debug)]
+pub enum HandleRequestError {
+    GeminiError(GeminiResponseError),
+    InvalidFormat(serde_json::Error),
+}
+impl Display for HandleRequestError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::GeminiError(e) => {
+                eprintln!("GeminiError: {:?}", e);
+                write!(f, "ERROR: Something went wrong! That all we know.")
+            }
+            Self::InvalidFormat(e) => {
+                write!(f, "ERROR: Invalid request format:\n{e}")
+            }
+        }
+    }
+}
+impl std::error::Error for HandleRequestError {}
+pub async fn handle_request(body: &str) -> Result<GeminiResponseStream, HandleRequestError> {
+    let request: ApiRequest = from_str(body).map_err(|e| HandleRequestError::InvalidFormat(e))?;
+    plan_tour(request.session)
+        .map_err(|e| HandleRequestError::GeminiError(e))
+        .await
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,109 @@
 mod api_requests;
-mod utils;
-mod function;
 mod constants;
+mod function;
+mod utils;
 
-fn main() {
-    // Load environment variables from .env file
-    dotenv::dotenv().ok();
+use crate::function::handle_request;
+use gemini_client_api::{futures::StreamExt, gemini::types::request::Role};
+use lambda_runtime::{
+    LambdaEvent, service_fn,
+    streaming::{Body, Response, channel},
+    tracing,
+};
+use serde::Deserialize;
+
+const CHUNK_SEPERATOR: &str = "\n";
+
+#[derive(Deserialize)]
+struct EventBody {
+    body: String,
+}
+
+async fn stream_handler(
+    event: LambdaEvent<EventBody>,
+) -> Result<Response<Body>, lambda_runtime::Error> {
+    let (mut tx, rx) = channel();
+    tokio::spawn(async move {
+        loop {
+            let response = handle_request(&event.payload.body).await;
+            match response {
+                Ok(mut response_stream) => {
+                    while let Some(gemini_response) = response_stream.next().await {
+                        match gemini_response {
+                            Ok(data) => {
+                                let response =
+                                    serde_json::to_string(data.get_chat().parts()).unwrap();
+                                print!("{response}");
+                                let chunk = format!("{response}{CHUNK_SEPERATOR}").into();
+                                tx.send_data(chunk).await.unwrap();
+                            }
+                            Err(error) => {
+                                eprintln!("ERROR: Did not send stream due to error:\n{error}")
+                            }
+                        }
+                    }
+                    if *response_stream
+                        .get_session()
+                        .get_last_chat()
+                        .unwrap()
+                        .role()
+                        != Role::Function
+                    {
+                        println!("Response streaming completed.");
+                        break;
+                    } else {
+                        println!("Resolving function calls.")
+                    }
+                }
+                Err(e) => {
+                    eprintln!("ERROR: handle_request failed:\n{e}");
+                    tx.send_data(e.to_string().into()).await.unwrap();
+                    break;
+                }
+            }
+        }
+    });
+    Ok(Response::from(rx))
+}
+
+#[tokio::main]
+async fn main() -> Result<(), lambda_runtime::Error> {
+    tracing::init_default_subscriber();
+    match lambda_runtime::run(service_fn(stream_handler)).await {
+        Ok(_) => {}
+        Err(e) => eprint!("Error:\n{e}"),
+    }
+    Ok(())
+}
+
+#[tokio::test]
+async fn stream_handler_test() {
+    use crate::function::ApiRequest;
+    use gemini_client_api::futures::StreamExt;
+    use gemini_client_api::gemini::types::sessions::Session;
+    use serde_json::to_string;
+
+    let mut session = Session::new(10);
+    session.ask_string("I want to travel to goa from ranchi");
+    let body = to_string(&ApiRequest { session }).unwrap();
+
+    let response = stream_handler(LambdaEvent {
+        payload: EventBody { body },
+        context: lambda_runtime::Context::default(),
+    })
+    .await
+    .unwrap();
+    let mut stream = response.stream;
+
+    // EXPLICITLY CONSUME THE STREAM
+    // This waits until 'tx' is dropped or closed in your spawn block
+    while let Some(chunk) = stream.next().await {
+        match chunk {
+            Ok(bytes) => {
+                let s = String::from_utf8_lossy(&bytes);
+                println!("Received chunk: {}", s);
+            }
+            Err(e) => panic!("Stream error: {:?}", e),
+        }
+    }
 }
